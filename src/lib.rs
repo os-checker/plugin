28c4afdedcf269f01a56bc6e003244c11085706a
zjp <jiping_zhou@foxmail.com>
2024-11-23 10:11:06 +0800

    reexport more items including crates themselves

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,15 @@ pub mod prelude {
     pub use camino::{Utf8Path, Utf8PathBuf};
     pub use cargo_metadata::Metadata;
     pub use compact_str::CompactString as XString;
+    pub use duct::cmd;
     pub use eyre::{Context, Result};
     pub use indexmap::IndexMap;
     pub use jiff::Timestamp;
+    pub use serde::{Deserialize, Serialize};
+
+    pub use cargo_metadata;
+    pub use duct;
+    pub use indexmap;
+    pub use serde;
+    pub use serde_json;
 }
