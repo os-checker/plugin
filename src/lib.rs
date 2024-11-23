0ab27a252365c267f5ab2961251aeefb0c2ec821
zjp <jiping_zhou@foxmail.com>
2024-11-23 10:28:04 +0800

    add write_json & repos functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub mod prelude {
     pub use cargo_metadata::Metadata;
     pub use compact_str::CompactString as XString;
     pub use duct::cmd;
-    pub use eyre::{Context, Result};
+    pub use eyre::{Context, ContextCompat, Result};
     pub use indexmap::IndexMap;
     pub use jiff::Timestamp;
     pub use serde::{Deserialize, Serialize};
@@ -15,4 +15,44 @@ pub mod prelude {
     pub use indexmap;
     pub use serde;
     pub use serde_json;
+}
+
+#[macro_use]
+extern crate tracing;
+
+use prelude::*;
+use std::fs;
+
+/// Write Serializable data to a file. This function creates all parent
+/// folders if they do not exist.
+pub fn write_json<T: Serialize>(path: &Utf8Path, val: &T) -> Result<()> {
+    let _span = error_span!("write_json", ?path).entered();
+
+    let parent = path.parent().with_context(|| "No parent path found.")?;
+    fs::create_dir_all(parent)?;
+
+    serde_json::to_writer_pretty(fs::File::create(path)?, val)?;
+    Ok(())
+}
+
+/// Get the list of repos, searching in the following order:
+/// * the first CLI argument: a json path to a vec of repo string
+/// * or read from the result of `os-checker config --list-repos`
+pub fn repos() -> Result<Vec<String>> {
+    let arg = std::env::args().nth(1);
+
+    let text = match arg.as_deref() {
+        Some(list_json) => {
+            let path = Utf8Path::new(list_json);
+            let _span = error_span!("repos", ?path).entered();
+            fs::read_to_string(path)?
+        }
+        None => duct::cmd!("os-checker", "config", "--list-repos")
+            .env_remove("RUST_LOG")
+            .read()
+            .with_context(|| "Failed to run `os-checker config --list-repos`")?,
+    };
+
+    info!(text);
+    Ok(serde_json::from_str(&text)?)
 }
