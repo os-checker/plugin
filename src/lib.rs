4cd26fa5b5cc7dbf0410478056f567785e650fe5
zjp <jiping_zhou@foxmail.com>
2024-11-23 08:02:44 +0800

    init: add logger and prelude modules

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod logger;
+
+pub mod prelude {
+    pub use camino::{Utf8Path, Utf8PathBuf};
+    pub use cargo_metadata::Metadata;
+    pub use compact_str::CompactString as XString;
+    pub use eyre::{Context, Result};
+    pub use indexmap::IndexMap;
+    pub use jiff::Timestamp;
+}
